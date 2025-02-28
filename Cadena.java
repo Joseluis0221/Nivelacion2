fcfa046913bc98874ee57c30624b0a0e5267020f
Joseluis0221 <Camiloobregon96@gmail.com>
2025-02-28 14:22:56 -0500

    Create Cadena.java

--- Cadena.java
+++ Cadena.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class InvertirCadena {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce una cadena: ");
+        String cadena = scanner.nextLine();
+        String invertida = "";
+
+        for (int i = cadena.length() - 1; i >= 0; i--) {
+            invertida += cadena.charAt(i);
+        }
+
+        System.out.println("Cadena invertida: " + invertida);
+    }
+}
 
